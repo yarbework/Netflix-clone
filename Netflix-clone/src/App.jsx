a2ff8d25e24ff08c0251f799393e0c543a68c3cb
Yarbework <yarbemohaz3517@gmail.com>
2025-08-01 13:45:19 +0300

    header and Footer

---
+++ Netflix-clone/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+import Home from "./Pages/Home";
+
+function App() {
+  return (
+    <>
+      <div className="App"></div>
+      <Home />
+    </>
+  );
+}
+
+export default App;
---
+++ Netflix-clone/src/Components/Header/Header.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./header.css";
+import NetflixLogo from "./netflix-logo-large.png";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+//we have to import search and notification icons from material icon
+const Header = () => {
+  return (
+    <div className="header_outer_container">
+      <div className="header_container">
+        <div className="header_left">
+          <ul>
+            <li>
+              <img src={NetflixLogo} alt="Netflix Logo" width={100} />
+            </li>
+            <li>Home</li>
+            <li>TV Shows</li>
+            <li>Movies</li>
+            <li>Latest</li>
+            <li>My Latest</li>
+            <li>Brows By Language</li>
+          </ul>
+        </div>
+        <div className="header_right">
+          <ul>
+            <li>
+              <SearchIcon />
+            </li>
+            <li>
+              <NotificationsIcon />
+            </li>
+            <li>
+              <AccountBoxIcon />
+            </li>
+            <li>
+              <ArrowDropDownIcon />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
